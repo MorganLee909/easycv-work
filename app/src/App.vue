471a1f4a61233d37859da6d12d7eee60a0b94a95
“Ivan <ivan.kordonets@virtuozzo.com>
2022-05-29 20:34:39 +0300

    animate moto

--- app/src/App.vue
+++ app/src/App.vue
@@ -11,7 +11,8 @@ import WizzardView from './components/WizzardView.vue'
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
 
   #app {
-    font-family: 'Inter', sans-serif;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    // font-family: 'Inter', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
--- app/src/components/MotoWizzard.vue
+++ app/src/components/MotoWizzard.vue
@@ -5,8 +5,11 @@
 <template>
   <div class="container-moto">
     <p class="moto colorBlack">Create the Best CV</p>
-    <p class="moto colorGreen">just in Several <br> Clicks</p>
-    <p class="get-started"> Get Started --> </p>
+    <div class="second-row-container">
+      <p class="moto colorGreen">in just Several <br> Clicks</p>
+      <p class="get-started"> Get Started --> </p>
+    </div>
+
   </div>
 </template>
 
@@ -18,8 +21,12 @@
     // flex-flow: column;
   }
 
+  .second-row-container{
+    overflow: hidden;
+  }
+
   .moto{
-    font-weight: 800;
+    font-weight: 900;
     font-size: 74px;
     line-height: 110px;
     text-transform: uppercase;
@@ -30,10 +37,69 @@
 
   .colorBlack{
     color: $black;
+    position: relative;
+    animation: firstBlock 1500ms;
+    // animation-delay: 0.5s;
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+    // transition: visibility 0s, opacity 0.5s linear;
   }
 
-  .colorGreen{
+    .colorGreen{
+    // display: inline-block;
+    position: relative;
+    transform: translateY(0%);
     color: $primary;
+    animation: secondBlock 1500ms ;
+    // animation: secondBlock 1200ms;
+    // animation-delay: 2s;
+    // animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+    // transition: opacity 5s, transform 5s;
+    // transition: visibility 0s, opacity 5s linear;
+
+  // &:before{
+  //   overflow: hidden;
+  //   top: -220px;
+  //   transition: all 1s;
+  // }
+
+  // &:after{
+  //   overflow: visible;
+  //   top: 0px;
+  //   transition: width 1s ease-in-out;
+  // }
+
+  }
+
+  // @for $i from 1 through 2 {
+  //     .moto {
+  //         animation-delay: #{$i * 2.5}s;
+  //     }
+  // }
+
+  @keyframes firstBlock {
+    0% {
+      opacity: 0;
+      top: -100%;
+      }
+    100% {
+      opacity: 1;
+      top: 0px
+      }
+  }
+
+  @keyframes secondBlock {
+    0% {
+      opacity: 0;
+      transform: translateY(-100%);
+      // transform: scaleX(10%);
+      // top: -100%;
+      }
+    100% {
+      // transform: scaleX(100%);
+      opacity: 1;
+      // top: 0px
+      // width: 100%;
+      }
   }
 
   .get-started{
