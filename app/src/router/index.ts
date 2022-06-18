b79079def8a5f807e566ee4e9bc203da863abe87
“Ivan <ivan.kordonets@virtuozzo.com>
2022-06-18 16:25:27 +0300

    add routes for: Sign In, Sign Up, Log Out and Home

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Wizzard',
+    name: 'Home',
     component: () => import(/* webpackChunkName: "sign up" */ '@/pages/WizzardView.vue'),
     alias: '/logout'
   },
