fc7872e27209113b81dd2de3e689338bbb2235af
Ivan Kordonets <ivan.kordonets@virtuozzo.com>
2022-07-13 13:10:14 +0300

    BaseToaster

--- app/src/pages/WizzardView.vue
+++ app/src/pages/WizzardView.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import HeaderMain from '../components/HeaderMain.vue'
 import MainContainer from '@/components/ContainerMain.vue'
+// console.log('VUE_APP_ROOT_API_PORT', process.env.VUE_APP_ROOT_API_PORT)
+
+// #if(${URL}) Access URL: ${URL}
+// #end Login: ${LOGIN}
+// Password: ${PASSWORD}
+
 </script>
 
 <template>
